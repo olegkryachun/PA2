401f69228830b28e968e7ec6c7da5d5c6b67245b
olegkryachun <39966228+olegkryachun@users.noreply.github.com>
2019-01-31 22:14:25 -0800

    Update stuff.h

--- stuff.h
+++ stuff.h
@@ -4,11 +4,20 @@ struct node{
 };
 
 
+typedef struct{
+  struct node *prev;
+  struct node *cur;
+  int found;
+  int empty;
+}Return;
+
 
 struct node *init();
 int add(struct node *list, int num);
 void print(struct node *list);
 int del(struct node* list, int num);
-int search(struct node *list, int num);
+Return search(struct node *list, int num);
 void printInfo();
 void start(struct node *list);
+
+
